c7aa3ad103a2003a5073313bf00b87b2ea56bf3e
bartcevich <bartcevich@tut.by>
2023-02-23 15:23:17 +0300

    start10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n=String(n);
+  let a='';
+  for (let i=n.length-1; i>=0; i--){
+    a=`${a}${n[i]}`;
+    console.log(a);
+  }
+  return parseInt(a);
 }
